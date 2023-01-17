48b67fc3dd659d0407308c2d98b17d745cebdec8
SherlockLena <slavinskaya.lena2001@gmail.com>
2023-01-18 00:38:08 +0200

    refactored with hooks

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,128 +1,120 @@
-import './charList.scss';
-import { Component } from 'react';
-import PropTypes from 'prop-types';
-import MarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
+import "./charList.scss";
+import { useState, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-class CharList extends Component {
-    state = {
-        charList: [],
-        loading: true,
-        error: false,
-        newItemLoading: false,
-        offset: 210,
-        isEnded: false
-    }
+const CharList = (props) => {
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(210);
+    const [isEnded, setIsEnded] = useState(false);
 
-    marvelService = new MarvelService();
+    const marvelService = new MarvelService();
 
-    componentDidMount() {
-        this.onRequest();
-    }
+    useEffect(() => {
+        onRequest();
+    }, []);
 
-    onRequest = (offset) => {
-        this.onCharListLoading();
-        this.marvelService.getAllCharacters(offset)
-            .then(this.onCharListLoaded)
-            .catch(this.onError);
-    }
+    const onRequest = (offset) => {
+        onCharListLoading();
+        marvelService
+            .getAllCharacters(offset)
+            .then(onCharListLoaded)
+            .catch(onError);
+    };
 
-    onCharListLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
-    } 
+    const onCharListLoading = () => {
+        setNewItemLoading(true);
+    };
 
-    onCharListLoaded = (newCharList) => {
+    const onCharListLoaded = (newCharList) => {
         let ended = false;
-        if(newCharList.length < 9) ended = true;
+        if (newCharList.length < 9) ended = true;
 
+        setCharList((charList) => [...charList, ...newCharList]);
+        setLoading(false);
+        setNewItemLoading(false);
+        setOffset((offset) => offset + 9);
+        setIsEnded(ended);
+    };
 
-        this.setState(({offset, charList}) => ({
-                charList: [...charList, ...newCharList],
-                loading: false,
-                newItemLoading: false,
-                offset: offset + 9,
-                isEnded: ended
-        }))
-    }
+    const onError = () => {
+        setError(true);
+        setLoading(false);
+    };
 
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
-    }
+    const itemRefs = useRef([]);
 
-    itemRefs = [];
+    const itemIsSelected = (i) => {
+        itemRefs.current.forEach((item) =>
+            item.classList.remove("char__item_selected")
+        );
+        itemRefs.current[i].classList.add("char__item_selected");
+        itemRefs.current[i].focus();
+    };
 
-    setRef = (ref) => {
-        this.itemRefs.push(ref);
-    }
-
-    itemIsSelected = (i) => {
-        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-        this.itemRefs[i].classList.add('char__item_selected');
-        this.itemRefs[i].focus();
-    }
-
-    renderItems(arr) {
-        const items =  arr.map((item, i) => {
-            let imgStyle = {'objectFit' : 'cover'};
-            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgStyle = {'objectFit' : 'unset'};
+    function renderItems(arr) {
+        const items = arr.map((item, i) => {
+            let imgStyle = { objectFit: "cover" };
+            if (
+                item.thumbnail ===
+                "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+            ) {
+                imgStyle = { objectFit: "unset" };
             }
-            
+
             return (
-                <li className="char__item"
-                    key={item.id} 
+                <li
+                    className="char__item"
+                    key={item.id}
                     onClick={() => {
-                        this.props.onCharSelected(item.id);
-                        this.itemIsSelected(i);
+                        props.onCharSelected(item.id);
+                        itemIsSelected(i);
                     }}
-                    ref={this.setRef}>
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                    ref={(el) => (itemRefs.current[i] = el)}
+                >
+                    <img
+                        src={item.thumbnail}
+                        alt={item.name}
+                        style={imgStyle}
+                    />
                     <div className="char__name">{item.name}</div>
                 </li>
-            )
+            );
         });
-        
-        return (
-            <ul className="char__grid">
-                {items}
-            </ul>
-        )
+
+        return <ul className="char__grid">{items}</ul>;
     }
 
-    render() {
-        const {charList, loading, error, offset, newItemLoading, isEnded} = this.state;
-        
-        const items = this.renderItems(charList);
+    const items = renderItems(charList);
 
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? items : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
 
-        return (
-            <div className="char__list">
-                {errorMessage}
-                {spinner}
-                {content}
-                <button 
-                    onClick={() => this.onRequest(offset)}
-                    disabled={newItemLoading}
-                    style={{'display': isEnded ? 'none' : 'block'}}
-                    className="button button__main button__long">
-                    <div className="inner">load more</div>
-                </button>
-            </div>
-        )
-    }
-}
+    return (
+        <div className="char__list">
+            {errorMessage}
+            {spinner}
+            {content}
+            <button
+                onClick={() => onRequest(offset)}
+                disabled={newItemLoading}
+                style={{ display: isEnded ? "none" : "block" }}
+                className="button button__main button__long"
+            >
+                <div className="inner">load more</div>
+            </button>
+        </div>
+    );
+};
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func.isRequired
-}
+    onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,100 +1,99 @@
-import { Component } from 'react';
-import './randomChar.scss';
-import mjolnir from '../../resources/img/mjolnir.png';
-import MarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
+import { useState, useEffect } from "react";
+import "./randomChar.scss";
+import mjolnir from "../../resources/img/mjolnir.png";
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-class RandomChar extends Component{
-    state = {
-        char: {},
-        loading: true,
-        error: false
-    }
+const RandomChar = () => {
+    const [char, setChar] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
-    marvelService = new MarvelService();
+    const marvelService = new MarvelService();
 
-    onCharLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false
-        });
-    }
+    const onCharLoaded = (char) => {
+        setChar(char);
+        setLoading(false);
+    };
 
-    componentDidMount() {
-        this.updateChar();
-        this.timerId = setInterval(this.updateChar, 15000);
-    }
+    useEffect(() => {
+        updateChar();
+        const timerId = setInterval(updateChar, 15000);
 
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    }
+        return () => {
+            clearInterval(timerId);
+        };
+    }, []);
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        });
-    }
+    const onError = () => {
+        setLoading(false);
+        setError(true);
+    };
 
-    onCharLoading = () => {
-        this.setState({
-            loading: true
-        })
-    }
+    const onCharLoading = () => {
+        setLoading(true);
+    };
 
-    updateChar = () => {
+    const updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        this.onCharLoading();
-        this.marvelService.getCharacter(id)
-            .then(this.onCharLoaded)
-            .catch(this.onError)
-    }
+        onCharLoading();
+        marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+    };
 
-    render() {
-        const { char, loading, error} = this.state;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = ! (loading || error) ? <View char={char}/> : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View char={char} /> : null;
 
-        return (
-            <div className="randomchar">
-                {errorMessage}
-                {spinner}
-                {content}
-                <div className="randomchar__static">
-                    <p className="randomchar__title">
-                        Random character for today!<br/>
-                        Do you want to get to know him better?
-                    </p>
-                    <p className="randomchar__title">
-                        Or choose another one
-                    </p>
-                    <button onClick={this.updateChar} className="button button__main">
-                        <div className="inner">try it</div>
-                    </button>
-                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-                </div>
+    return (
+        <div className="randomchar">
+            {errorMessage}
+            {spinner}
+            {content}
+            <div className="randomchar__static">
+                <p className="randomchar__title">
+                    Random character for today!
+                    <br />
+                    Do you want to get to know him better?
+                </p>
+                <p className="randomchar__title">Or choose another one</p>
+                <button
+                    onClick={updateChar}
+                    className="button button__main"
+                >
+                    <div className="inner">try it</div>
+                </button>
+                <img
+                    src={mjolnir}
+                    alt="mjolnir"
+                    className="randomchar__decoration"
+                />
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
-const View = ({char}) => {
-    const  {name, description, thumbnail, homepage, wiki} = char;
-    let imgStyle = {'objectFit' : 'cover'};
-    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-        imgStyle = {'objectFit' : 'contain'};
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+    let imgStyle = { objectFit: "cover" };
+    if (
+        thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+    ) {
+        imgStyle = { objectFit: "contain" };
     }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
+            <img
+                src={thumbnail}
+                alt="Random character"
+                className="randomchar__img"
+                style={imgStyle}
+            />
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
-                <p className="randomchar__descr">
-                    {description}
-                </p>
+                <p className="randomchar__descr">{description}</p>
                 <div className="randomchar__btns">
                     <a href={homepage} className="button button__main">
                         <div className="inner">homepage</div>
@@ -106,6 +105,6 @@ const View = ({char}) => {
             </div>
         </div>
     );
-}
+};
 
 export default RandomChar;
