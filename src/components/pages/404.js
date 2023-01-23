a70918fc05313281007db9ccf5e813589da6bf9b
SherlockLena <slavinskaya.lena2001@gmail.com>
2023-01-23 23:45:08 +0200

    helmet is added

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,17 @@
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Page404 = () => {
     return(
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="error 404 page not found"
+                    />
+                <title>Page not found</title>
+            </Helmet>
             <ErrorMessage/>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Sorry, page doesn't exist</p>
             <Link style={{'display':'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize':'24px',
